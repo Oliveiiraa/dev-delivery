022f472b13bb036179f8f3cc36f589ae2db71e5b
Gabriel Oliveira <gabrieloliveira@pop-os.localdomain>
2022-10-22 21:08:52 -0300

    feat: first commit

---
+++ pages/[tenant]/index.tsx
@@ -0,0 +1,27 @@
+import styles from "../../styles/Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.headerTop}>
+          <div className={styles.headerTopLeft}>
+            <div className={styles.headerTitle}>Seja Bem-Vindo(a) 👋</div>
+            <div className={styles.headerSubtitle}>O que deseja para hoje?</div>
+          </div>
+
+          <div className={styles.headerTopRight}>
+            <div className={styles.menuButton}>
+              <div className={styles.menuButtonLine}></div>
+              <div className={styles.menuButtonLine}></div>
+              <div className={styles.menuButtonLine}></div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.headerBottom}>...</div>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
