22d86b2d645973da492dda070757705c41fa61c2
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2023-02-12 16:23:44 -0300

    feat: add Context Tenent

---
+++ contexts/AppContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+import { Tenant } from "../@types/Tenent";
+
+type AppContextType = {
+  tenant: Tenant | null;
+  setTenant: (newTenant: Tenant) => void;
+};
+
+const defaultTenant: AppContextType = {
+  tenant: null,
+  setTenant: () => null,
+};
+
+const appContext = createContext<AppContextType>(defaultTenant);
+
+export const useAppContext = () => useContext(appContext);
+
+type Props = {
+  children: ReactNode;
+};
+export const AppContextProvider = ({ children }: Props) => {
+  const [tenant, setTenant] = useState<Tenant | null>(null);
+
+  return (
+    <appContext.Provider value={{ tenant, setTenant }}>
+      {children}
+    </appContext.Provider>
+  );
+};
